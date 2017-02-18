ba48fc6a6d5bfa0bd0d3cebc9b10c2032bad2834
vgty <victor.goutay@gmail.com>
2017-02-18 18:41:52 +0200

    Refactoring select method

--- filedb/lib/file_db/table.rb
+++ filedb/lib/file_db/table.rb
@@ -10,19 +10,24 @@ module FileDb
     def select(arg="")
       if arg.is_a? String
         data[category]  
-      elsif arg.has_key?(:where)        
-        if arg[:where].has_key?(:id)
-          data[category][arg[:where][:id] - 1]               
-        elsif arg[:where].has_key?(:director_id)
-          director = arg[:where][:director_id]
+      elsif arg.has_key?(:where)
+         searching = arg[:where].keys
+        if searching.include?(:id)
+          movie_index = (arg[:where][:id]) - 1
+          data[category][movie_index]               
+        else 
+          searching[0]
+          key_name = arg[:where].keys[0]
+          key_value = arg[:where][key_name]
           arr =[]
           data[category].each do |movie|
-            arr << movie if movie["director_id"] == director
+            arr << movie if movie["#{key_name}"] == key_value
           end
           arr
         end        
       end
     end
+
     
     def insert(new_movie)
       id_generator(new_movie)
--- filedb/test/file_db/table_test.rb
+++ filedb/test/file_db/table_test.rb
@@ -26,15 +26,36 @@ class FileDb::TableTest < Minitest::Test
     assert_equal(arr, movies.select(where: { director_id: 1}))
   end
   
+  def test_table_returns_movies_sorted_by_year
+    arr = []
+    db.data["movies"].each do |movie|
+      if movie["year"] == 1926
+      arr << movie
+      end
+    end
+    assert_equal(arr, movies.select(where: { year: 1926}))
+  end
+  
+  def test_table_returns_movies_sorted_by_title
+    arr = []
+    db.data["movies"].each do |movie|
+      if movie["title"] == 'Psycho'
+      arr << movie
+      end
+    end
+    assert_equal(arr, movies.select(where: { title: "Psycho"}))
+  end
+  
   def test_table_returns_movies_with_new_movie
     new_list = db.data["movies"].dup
     data_length = db.data["movies"].length
     new_movie = { title: "Birds", year: 1962, director_id: 2 }.merge!(id: "#{data_length}".to_i + 1)
     new_list << new_movie
-    assert_equal(new_list, @db.table.insert({ title: "Birds", year: 1962, director_id: 2 }))
+    assert_equal(new_list, movies.insert({ title: "Birds", year: 1962, director_id: 2 }))
   end
   
-  def test_table_returns_movies_with_movie_updated
-    new_list = db.data["movies"].dup
-  end
+  # def test_table_returns_movies_with_movie_updated
+  #   db.data["movies"][7]
+  #   assert_equal(, movies.update(where: { id: 7 }, values: { year: 1963 }))
+  # end
 end
